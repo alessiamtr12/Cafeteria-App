2c79e72531c53e07a939cb35fe42646edb562cfb
Alessia Mitrica <alessia.mitrica12@gmail.com>
2026-02-27 18:46:39 +0200

    create menu updated

--- src/main/java/org/example/helloworld/model/MenuComponent.java
+++ src/main/java/org/example/helloworld/model/MenuComponent.java
@@ -8,7 +8,7 @@ import javax.persistence.*;
 public abstract class MenuComponent {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
